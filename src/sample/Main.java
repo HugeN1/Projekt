343c7ee6dae8da20c4efa28a895c66c50bae0820
HugeN1 <43601485+HugeN1@users.noreply.github.com>
2020-06-08 10:07:16 +0200

    Add files via upload

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,9 +9,11 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.scene.text.Font;
@@ -20,19 +22,18 @@ import javafx.util.Duration;
 
 public class Main extends Application {
 
-    private static final int iloscPar = 8;
-    private static final int liczbaWierszy = 4;
+    private static final int iloscPar = 18;
+    private static final int liczbaWierszy = 6;
     private Karta wybrany = null;
     private int licznikKlikniec = 2;
-    static int pary=0;
+    static int pary = 0;
 
-    private Parent tworzenieZawartosci()
-    {
-        Pane rdzen = new Pane();
-        rdzen.setPrefSize(400,400);
+    private Parent tworzenieZawartosci() {
+        Pane root = new Pane();
+        root.setPrefSize(600, 600);
         char a = 'A';
         List<Karta> karty = new ArrayList<>();
-        for (int i = 0; i< iloscPar; i++){
+        for (int i = 0; i < iloscPar; i++) {
             karty.add(new Karta(String.valueOf(a)));
             karty.add(new Karta(String.valueOf(a)));
             a++;
@@ -40,27 +41,28 @@ public class Main extends Application {
 
         Collections.shuffle(karty);
 
-        for (int i = 0; i< karty.size(); i++){
-            Karta karta =karty.get(i);
+        for (int i = 0; i < karty.size(); i++) {
+            Karta karta = karty.get(i);
             karta.setTranslateX(100 * (i % liczbaWierszy));
             karta.setTranslateY(100 * (i / liczbaWierszy));
-            rdzen.getChildren().add(karta);
+            root.getChildren().add(karta);
         }
 
-        return rdzen;
+        return root;
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Memory PC");
         primaryStage.setScene(new Scene(tworzenieZawartosci()));
         primaryStage.show();
     }
 
-    private class Karta extends StackPane{
+    private class Karta extends StackPane {
         private Text tekst = new Text();
-        public Karta(String wartość){
-            Rectangle border = new Rectangle(100,100);
+
+        public Karta(String wartość) {
+            Rectangle border = new Rectangle(100, 100);
             border.setFill(null);
             border.setStroke(Color.BLACK);
             tekst.setText(wartość);
@@ -72,19 +74,19 @@ public class Main extends Application {
             {
                 if (jestOtwarte() || licznikKlikniec == 0)
                     return;
-                licznikKlikniec --;
-                if (wybrany == null){
+                licznikKlikniec--;
+                if (wybrany == null) {
                     wybrany = this;
-                    otwarte(()-> {});
-                }
-                else{
-                    otwarte(()->{if(!taSamaWartosc(wybrany)){
-                        wybrany.zamknięte();
-                        this.zamknięte();
-                    }
-                    else{
-                        pary++;
-                    }
+                    otwarte(() -> {
+                    });
+                } else {
+                    otwarte(() -> {
+                        if (!taSamaWartosc(wybrany)) {
+                            wybrany.zamknięte();
+                            this.zamknięte();
+                        } else {
+                            pary++;
+                        }
 
                         wybrany = null;
                         licznikKlikniec = 2;
@@ -94,22 +96,25 @@ public class Main extends Application {
 
             zamknięte();
         }
-        public boolean jestOtwarte(){
+
+        public boolean jestOtwarte() {
             return tekst.getOpacity() == 1;
         }
-        
-        public void otwarte(Runnable action){
+
+        public void otwarte(Runnable action) {
             FadeTransition ft = new FadeTransition(Duration.seconds(0.5), tekst);
             ft.setToValue(1);
             ft.setOnFinished(event -> action.run());
             ft.play();
         }
-        public void zamknięte(){
+
+        public void zamknięte() {
             FadeTransition ft = new FadeTransition(Duration.seconds(0.1), tekst);
             ft.setToValue(0);
             ft.play();
         }
-        public boolean taSamaWartosc(Karta inna){
+
+        public boolean taSamaWartosc(Karta inna) {
 
             return tekst.getText().equals(inna.tekst.getText());
         }
@@ -122,7 +127,7 @@ public class Main extends Application {
     }
 
     private static void wygrana() {
-        if(pary==iloscPar){
+        if (pary == iloscPar) {
             System.out.println("wygrałeś");
         }
     }
